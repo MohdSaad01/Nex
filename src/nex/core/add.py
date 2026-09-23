e6495e3eaaac5bcb69b52c31cb895c3243706631
Mohd Saad Ansari <ansari.mohdsaad25@gmail.com>
2026-09-24 01:22:26 +0530

    feat: ignore files listed in .nexignore

--- src/nex/core/add.py
+++ src/nex/core/add.py
@@ -7,7 +7,24 @@ def adjoin(filename, **kwargs):
     file_exits = any(search_dir.rglob(filename))
 
     if file_exits:
-        print("File exits")
+        if check_ignore(filename):
+            print("Hashing it")
+        else:
+            print("File in .nexignore")
     else:
         print("File doesn't exits")
 
+def check_ignore(filename):
+    ignore = ".nexignore"
+
+    temp_ignore = Path(ignore).read_text(encoding="utf-8").splitlines()
+
+    ignored_list = []
+    for line in temp_ignore:
+        if not line.startswith("#") and len(line) > 0:
+            ignored_list.append(line)
+
+    if filename not in ignored_list:
+        return True
+
+    return None
